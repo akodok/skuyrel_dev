7e55a312ee7c1aeb8d8726421a4070bd9a090a54
Driss <driss.lgi@gmail.com>
2025-10-06 12:12:13 +0200

    Modification de l'access_token

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -56,7 +56,7 @@ router.post("/", async (req, res) => {
         res.cookie("access_token", token, {
           httpOnly: true,
           secure: false,           // true en prod (HTTPS)
-          sameSite: "lax",
+          sameSite: "none",
           maxAge: ttl * 1000,
           path: "/",
         });
