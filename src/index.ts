1ad67ab57887a5e74f0eb5178af2e94996396491
Driss <driss.lgi@gmail.com>
2025-10-06 12:04:02 +0200

    CORS

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://127.0.0.1:5173",
+      "http://localhost:5173",
       "http://15.188.26.20:3000"
     ],
     credentials: true
